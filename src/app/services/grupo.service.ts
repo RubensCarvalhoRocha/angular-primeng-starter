c10a59f478e7579babf2551ee1903193889b0064
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-09-18 21:46:41 -0300

    implementando formulário de grupo

--- src/app/services/grupo.service.ts
+++ src/app/services/grupo.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from '../../environment/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GrupoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createGrupo(grupo: any): Observable<any> {
+    return this.http.post<any>(`${environment.api}/criar`, grupo);
+  }
 }
